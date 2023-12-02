3d63464cad07fd9388d343775a9f2dbbdfd26164
jenleap <jenleap@gmail.com>
2023-12-01 19:13:52 -0500

    added infinite scroll

--- src/components/product-page.component.jsx
+++ src/components/product-page.component.jsx
@@ -5,22 +5,56 @@ import ProductItem from "./partials/product-item.component";
 import { Modal } from "./ui/modal.component";
 import { Container } from "./ui/container.styled";
 import { formatPrice } from "../utils/formatters";
-import { BasicText, NumberText, SubHeadingText } from "./ui/text.styled";
+import { BasicText, NumberText } from "./ui/text.styled";
 
 export const ProductPage = () => {
     const [ products, setProducts ] = useState([]);
     const [ showProductModal, setShowProductModal ] = useState(false);
     const [ selectedProduct, setSelectedProduct ] = useState(undefined);
+    const [ isLoading, setIsLoading] = useState(false);
     const [page, setPage] = useState(1);
 
+    const handleScroll = () => {
+        const endOfPage = window.innerHeight + window.pageYOffset >= document.body.offsetHeight;
+        if (endOfPage && products.length < 100 && !isLoading) {
+            console.log(products.length);
+            getProducts();
+        }
+    };
+
     useEffect(() => {
+        console.log("HERE");
         getProducts();
     }, []);
 
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [isLoading]);
+
     const getProducts = () => {
-        fetch(mockProductsApi)
-            .then(res => res.json())
-            .then(products => setProducts(products));
+        if (!isLoading) {
+            setIsLoading(true);
+
+            fetch(mockProductsApi)
+                .then(res => res.json())
+                .then(products => {
+                    console.log("PAGE: ", page);
+                    const formattedProducts = formatProductId(products);
+                    setProducts(prevProducts => [...prevProducts, ...formattedProducts]);
+                    setPage(prevPage => prevPage + 1);
+                    setIsLoading(false);
+                });
+        } 
+    }
+
+    const formatProductId = (products) => {
+        return products.map(product => {
+            return {
+                ...product,
+                id: `${product.id}-${page}`
+            }
+        });
     }
 
     const openProductModal = (product) => {
@@ -35,32 +69,27 @@ export const ProductPage = () => {
 
     return (
         <PageBackground>
-        <Container>
-        {
-            (products.length > 0) && (
-                <ProductGrid>
-                {
-                    products.map(product => (
-                        <ProductGridItem key={ product.id } onClick={ () => openProductModal(product)}>
-                            <ImageDiv  src={ product.image } alt={ product.title } />
-                            <DetailsDiv>
-                                <BasicText>{product.title}</BasicText>
-                                <NumberText>{formatPrice(product.price)}</NumberText>
-                            </DetailsDiv> 
-                        </ProductGridItem>
-                        
-                    ))
-                }
-                </ProductGrid>
-            )
-        }
-        <Modal 
-            showModal={ showProductModal }
-            closeModal={ closeProductModal }
-        >
-            <ProductItem product={ selectedProduct } onAdded={ closeProductModal }></ProductItem>
-        </Modal>
-        </Container>
+            <Container>
+                {(products.length > 0) && (
+                    <ProductGrid>
+                        {products.map(product => (
+                            <ProductGridItem key={ product.id } onClick={ () => openProductModal(product)}>
+                                <ImageDiv  src={ product.image } alt={ product.title } />
+                                <DetailsDiv>
+                                    <BasicText>{product.title}</BasicText>
+                                    <NumberText>{formatPrice(product.price)}</NumberText>
+                                </DetailsDiv> 
+                            </ProductGridItem>
+                        ))}
+                    </ProductGrid>
+                )}
+                {(products.length >= 100) && <BasicText style={{ padding: '20px 0'}}>END OF PAGE</BasicText>}
+                <Modal 
+                    showModal={ showProductModal }
+                    closeModal={ closeProductModal }>
+                    <ProductItem product={ selectedProduct } onAdded={ closeProductModal }></ProductItem>
+                </Modal>
+            </Container>
         </PageBackground>
     )
 }
@@ -88,6 +117,11 @@ const ProductGridItem = styled.div`
     align-item: center;
     justify-content: space-between;
     border: 1px solid lightgrey;
+    transition: all 200ms ease-in-out;
+    &:hover {
+        cursor: pointer;
+        box-shadow: 0 5px 16px rgba(0,0,0, 0.2);
+    }  
 `;
 
 const ImageDiv = styled.img`
